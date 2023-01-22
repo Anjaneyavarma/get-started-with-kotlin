58f361d327f8959e888d8440626ce0f8e333f6bc
Varma <anjivarma.penmatsa@gmail.com>
2023-01-22 18:01:35 +0530

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun somemethod(a: Int){
+    var string: String = "Hello World";
+    println("the integer value is $a");
+    println("the string is $string");
+}
+
+fun main(){
+    somemethod(1);
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,13 @@
+var s:String ="Hello World";
+val s1:String="Varma";
+var b: Boolean = true;
+var age: Int = 30
+var name = 40
+
+
+fun main(){
+    //jj
+    s = "Hi"
+    println("s == $s, s1 == $s1, b == $b, age == $age, name == $name");
+}
+
